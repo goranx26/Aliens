5f9f4b868cbcff07194349bbdd8fc09fae73a4cf
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-08-10 10:24:07 +0000

    Movement speed can be decimal value

--- ship.py
+++ ship.py
@@ -2,18 +2,37 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """initialize the ship and set starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load ship image and get its rect
-        self.image = pygame.image.load('images/gube.bmp')
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         # start each new ship at bottom center
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+        # movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ships position based on the movement flag."""
+        # Update the ships position based on the center value, not the rect
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """draw the ship at its current location"""
