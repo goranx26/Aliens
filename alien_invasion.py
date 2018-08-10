f2f84d311894ff9ddb8bdbdbabce323af4e70779
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-08-10 11:42:21 +0000

    In progress to fire bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 
 def run_game():
@@ -12,16 +13,20 @@ def run_game():
 
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
+    pygame.display.set_caption("Pirate attack")
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+
+    # Make a group to store bullets in
+    bullets = Group()
 
     # Start the mainloop for the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
