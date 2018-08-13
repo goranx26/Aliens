7158cf75b18add951708b3d89be0c30dfdcd4388
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-08-13 22:48:21 +0200

    'q' for quit

--- game_functions.py
+++ game_functions.py
@@ -46,7 +46,7 @@ def check_events(ai_settings, screen, ship, bullets):
 
 
 def update_screen(ai_settings, screen, ship, bullets):
-    """Updates the images on the screen and flip to the new screen"""
+    """Update the images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
 
