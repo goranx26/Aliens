d08abaf0e942e191c68d47da104ac588c3c85042
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-08-31 11:29:16 +0200

    Made the play button work

--- game_functions.py
+++ game_functions.py
@@ -71,7 +71,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Responds to keypresses and mouse events"""
 
     for event in pygame.event.get():
@@ -83,6 +83,16 @@ def check_events(ai_settings, screen, ship, bullets):
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Start a new game when player clicks play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
@@ -100,7 +110,7 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
     # Draw the play button if the game is inactive
     if not stats.game_active:
         play_button.draw_button()
-        
+
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
