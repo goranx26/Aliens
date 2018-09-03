d89941035f6a55f4dce34b9b21ea0452bfb2818f
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-09-03 11:23:13 +0200

    Added scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 from pygame.sprite import Group
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 
@@ -20,8 +21,9 @@ def run_game():
     # Make the play button
     play_button = Button(ai_settings, screen, "Play!")
 
-    # Create an instance to store the game stats
+    # Create an instance to store the game stats and create a scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Make a ship, a group of aliens and a group of bullets
     ship = Ship(ai_settings, screen)
@@ -40,7 +42,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
