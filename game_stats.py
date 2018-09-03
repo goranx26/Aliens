32a34bbf3da5c5eb3c73ceaa2d53c080f783d757
goranx26 <39906229+goranx26@users.noreply.github.com>
2018-09-03 13:53:50 +0200

    Showing the level

--- game_stats.py
+++ game_stats.py
@@ -15,5 +15,7 @@ class GameStats():
         """initialize statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0 
+        self.level = 1
+        
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,7 @@ class Scoreboard():
         # Prepare the intial score image
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rednered image"""
@@ -42,7 +43,17 @@ class Scoreboard():
 
 
     def show_score(self):
-        """Draw the score to screen"""
+        """Draw the scores and ships to screen"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
         
+
+    def prep_level(self):
+        """Turn the level into a rendered image"""
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+        # Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
